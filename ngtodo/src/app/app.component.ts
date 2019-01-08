dbad2b2a230a3fdb9797284b578dad8fbf29a14d
warnov <warnov@hotmail.com>
2019-01-08 00:30:49 -0500

    ngtodo mvp finished w 2way binding, filtering and inserting enities

--- ngtodo/src/app/app.component.ts
+++ ngtodo/src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from "@angular/core";
-import { Model } from "./model";
+import { Component } from '@angular/core';
+import { Model, TodoItem } from "./model";
+
+
 @Component({
-    selector: "todo-app",
-    templateUrl: "app.component.html"
+  selector: 'todo-app',
+  templateUrl: './app.component.html'
 })
+
+
 export class AppComponent {
-    model = new Model();
-    getName() {
-        return this.model.user;
+  model = new Model();
+
+  getName(){
+    return this.model.user;
+  }
+
+  getTodoItems(){
+    return this.model.items.filter(item=>!item.done);
+  }
+
+  addItem(newItem){
+    if(newItem!=""){
+      this.model.items.push(new TodoItem(newItem, false));
     }
-    getTodoItems(){
-        return this.model.items;
-    }
+  }
 }
